a4b17ae4aacc2b379d3caf19af77981b9041ee96
bigfarts <bigfarts@punymail.com>
2022-04-13 14:46:37 -0700

    actually just don't request adapter options

--- src/game.rs
+++ src/game.rs
@@ -93,11 +93,6 @@ impl Game {
                 mgba::gba::SCREEN_HEIGHT,
                 surface_texture,
             )
-            .request_adapter_options(pixels::wgpu::RequestAdapterOptions {
-                power_preference: pixels::wgpu::PowerPreference::LowPower,
-                force_fallback_adapter: false,
-                compatible_surface: None,
-            })
             .build()?;
             let gui = gui::Gui::new(
                 config,
