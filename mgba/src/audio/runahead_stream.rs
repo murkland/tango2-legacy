a450c33389f62a65e248d4b7554cf1c64b5d8f3d
bigfarts <bigfarts@punymail.com>
2022-04-16 00:11:47 -0700

    fix runahead stream

--- mgba/src/audio/runahead_stream.rs
+++ mgba/src/audio/runahead_stream.rs
@@ -1,46 +1,62 @@
-pub fn fill_buf(
-    buf: &mut Vec<i16>,
-    n: usize,
+pub struct RunaheadStream {
     core: std::sync::Arc<parking_lot::Mutex<crate::core::Core>>,
-    channels: u16,
     sample_rate: cpal::SampleRate,
-) -> usize {
-    let mut core = core.as_ref().lock();
-    let frame_count = (n / channels as usize) as u64;
+    channels: u16,
+}
 
-    if n > buf.len() {
-        *buf = vec![0i16; n];
+impl RunaheadStream {
+    pub fn new(core: std::sync::Arc<parking_lot::Mutex<crate::core::Core>>) -> RunaheadStream {
+        Self {
+            core,
+            sample_rate: cpal::SampleRate(0),
+            channels: 2,
+        }
     }
+}
 
-    let clock_rate = core.as_ref().frequency();
+impl super::Stream for RunaheadStream {
+    fn fill(&mut self, buf: &mut [i16]) -> usize {
+        let mut core = self.core.as_ref().lock();
+        let frame_count = (buf.len() / self.channels as usize) as u64;
 
-    let n = frame_count as i32;
+        let clock_rate = core.as_ref().frequency();
 
-    let mut buf_left = &mut buf[..];
+        let n = frame_count as i32;
 
-    while !buf_left.is_empty() {
-        let available = {
-            let mut core = core.as_mut();
-            let mut left = core.audio_channel(0);
-            left.set_rates(clock_rate as f64, sample_rate.0 as f64);
-            let mut available = left.samples_avail();
-            if available > n {
-                available = n;
+        let mut buf_left = &mut buf[..];
+
+        while !buf_left.is_empty() {
+            let available = {
+                let mut core = core.as_mut();
+                let mut left = core.audio_channel(0);
+                left.set_rates(clock_rate as f64, self.sample_rate.0 as f64);
+                let mut available = left.samples_avail();
+                if available > n {
+                    available = n;
+                }
+                left.read_samples(buf_left, available, self.channels == 2);
+                available
+            };
+
+            if self.channels == 2 {
+                let mut core = core.as_mut();
+                let mut right = core.audio_channel(1);
+                right.set_rates(clock_rate as f64, self.sample_rate.0 as f64);
+                right.read_samples(&mut buf_left[1..], available, self.channels == 2);
             }
-            left.read_samples(buf_left, available, channels == 2);
-            available
-        };
 
-        if channels == 2 {
-            let mut core = core.as_mut();
-            let mut right = core.audio_channel(1);
-            right.set_rates(clock_rate as f64, sample_rate.0 as f64);
-            right.read_samples(&mut buf_left[1..], available, channels == 2);
+            buf_left = &mut buf_left[(available * 2) as usize..];
+            core.as_mut().run_frame();
         }
 
-        buf_left = &mut buf_left[(available * 2) as usize..];
-        core.as_mut().run_frame();
+        buf.len()
     }
 
-    buf.len()
+    fn set_sample_rate(&mut self, sample_rate: cpal::SampleRate) {
+        self.sample_rate = sample_rate
+    }
+
+    fn set_channels(&mut self, channels: u16) {
+        self.channels = channels
+    }
 }
